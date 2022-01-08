8a3096224abf42e807fc5cfc577ed5e1517ac971
electronstogo <electronstogo@posteo.de>
2022-01-08 09:09:57 +0100

    Update dcf77.h

--- dcf77.h
+++ dcf77.h
@@ -10,20 +10,20 @@
 
 class DCF77
 {
-	private:
-		void calculate_time_from_dcf77_data(uint8_t* dcf_data, uint32_t last_signal_timestamp);
-		void handle_dcf_signal(int16_t signal_low_time, uint16_t* dcf_data_index, uint8_t* dcf_data);
-		uint8_t get_bit_value(uint8_t *data, uint16_t index);
+    private:
+        void calculate_time_from_dcf77_data(uint8_t* dcf_data, uint32_t last_signal_timestamp);
+        void handle_dcf_signal(int16_t signal_low_time, uint16_t* dcf_data_index, uint8_t* dcf_data);
+        uint8_t get_bit_value(uint8_t *data, uint16_t index);
 
-	public:
-		DCF77();
-		bool syncronize_time();
-		uint8_t minutes;
-		uint8_t hours;
-		uint8_t week_day;
-		uint8_t day_of_month;
-		uint8_t month;
-		uint8_t year;
+    public:
+        DCF77();
+        bool syncronize_time();
+        uint8_t minutes;
+        uint8_t hours;
+        uint8_t week_day;
+        uint8_t day_of_month;
+        uint8_t month;
+        uint8_t year;
 };
 
 
