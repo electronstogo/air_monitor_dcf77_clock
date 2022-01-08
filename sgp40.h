ccde83deaa30e345a50e03cb1e13b5ef7f68c784
electronstogo <electronstogo@posteo.de>
2022-01-08 09:19:21 +0100

    Update sgp40.h

--- sgp40.h
+++ sgp40.h
@@ -7,23 +7,23 @@ extern "C" {
 #include "sensirion_voc_algorithm.h"
 };
 
-#define SGP40_I2C_ADDRESS 		0x59
+#define SGP40_I2C_ADDRESS       0x59
 #define SGP40_CRC8_POLYNOMIAL 	0x31
 
 
 class SGP40Sensor
 {
-	private:
-		uint8_t generate_crc(uint8_t *data, uint16_t data_length);
-		bool do_raw_measurement(float temperature, float humidity, uint16_t* raw_value);
+    private:
+        uint8_t generate_crc(uint8_t *data, uint16_t data_length);
+        bool do_raw_measurement(float temperature, float humidity, uint16_t* raw_value);
 
 
-		// voc algorithm parameters needed for Sensirion VOC index algorithm.
-		VocAlgorithmParams voc_algorithm_parameters;
+        // voc algorithm parameters needed for Sensirion VOC index algorithm.
+        VocAlgorithmParams voc_algorithm_parameters;
 
-	public:
-		SGP40Sensor();
-		bool get_voc_index(float temperature, float humidity, uint16_t* voc_index);
+    public:
+        SGP40Sensor();
+        bool get_voc_index(float temperature, float humidity, uint16_t* voc_index);
 };
 
 
